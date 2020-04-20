766ad78fc214d0b67322c918b3024268bcd7b5a5
jebitok-dev <sharonkosgei4@gmail.com>
2020-04-20 14:19:25 +0300

    CSS Variables , React components

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,10 +3,12 @@ import Option from './Option';
 
 const Options = (props) => (
   <div>
-    <button onClick={props.handleDeleteOptions}>Remove All</button>
-    {props.options.length === 0 && <p>Please add an option to get started!</p>}
-    {
-      props.options.map((option) => (
+  <div className="widget-header">
+    <h3 className="widget-header_title">Your Options</h3>
+    <button className="button button--link" onClick={props.handleDeleteOptions}>Remove All</button>
+    </div>
+    {props.options.length === 0 && <p className="widget_message">Please add an option to get started!</p>}
+    {props.options.map((option) => (
         <Option
           key={option}
           optionText={option}
