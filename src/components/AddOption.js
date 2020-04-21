8d2053ed6fa5ce5cc12190b630555ca6a60d4ee7
jebitok-dev <sharonkosgei4@gmail.com>
2020-04-21 12:31:24 +0300

    sass components & 3rd party modules css

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -18,9 +18,9 @@ export default class AddOption extends React.Component {
   render() {
     return (
       <div>
-        {this.state.error && <p>{this.state.error}</p>}
+        {this.state.error && <p className="add-option-error">{this.state.error}</p>}
         <form onSubmit={this.handleAddOption}>
-          <input type="text" name="option" />
+          <input className="add-option-input" type="text" name="option" />
           <button className="button">Add Option</button>
         </form>
       </div>
