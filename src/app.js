2224b282f9a9693e2613ad8987d41f72dd66a1b0
Jebitok <sharonkosgei4@gmail.com>
2021-06-06 12:33:50 +0300

    Add Changes to Config

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "normalize.css/normalize.css";
 import './styles/styles.scss';
-import TodoApp from './TodoApp';
+import TodoApp from './components/TodoApp';
 
 ReactDOM.render(<TodoApp />, document.getElementById('app'));
